f2eb6de21028207c5451de8980d7e628e70f6950
Ciobanu Stanislav <stanislav.ciobanu@iis.utm.md>
2026-06-04 15:42:38 +0300

    Auth using seeded users from BD

--- wmsBack/src/main/java/com/isd/wms/controller/AuthController.java
+++ wmsBack/src/main/java/com/isd/wms/controller/AuthController.java
@@ -41,6 +41,9 @@ public class AuthController {
                     new UsernamePasswordAuthenticationToken(usernameOrEmail, password)
             );
         } catch (BadCredentialsException e) {
+
+            log.error("Detailed auth error: ", e);
+
             log.warn("Authentification failed for '{}': Incorrect credentials.", usernameOrEmail);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("error", "Incorrect username or password."));
