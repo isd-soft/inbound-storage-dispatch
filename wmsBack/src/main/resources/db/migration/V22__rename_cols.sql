7ec61fd74cd64395ed120e50409eb1cf973f77d4
whatyz <granaci55@gmail.com>
2026-06-13 10:50:55 +0300

    fixed migrations

---
+++ wmsBack/src/main/resources/db/migration/V22__rename_cols.sql
@@ -0,0 +1,29 @@
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'locations'
+          AND column_name = 'location_code'
+    ) AND NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'locations'
+          AND column_name = 'barcode'
+    ) THEN
+        ALTER TABLE locations RENAME COLUMN location_code TO barcode;
+    END IF;
+END $$;
+
+ALTER TABLE locations
+    ADD COLUMN IF NOT EXISTS name VARCHAR(50);
+
+UPDATE locations
+SET name = barcode
+WHERE name IS NULL;
+
+ALTER TABLE locations
+    ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE locations
+    ADD CONSTRAINT locations_name_key UNIQUE (name);
