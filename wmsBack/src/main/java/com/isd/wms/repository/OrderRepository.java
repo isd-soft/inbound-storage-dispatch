ed552035cbcc758f0cac6be7252b9bd831efeb4d
Linage <p.anastasia.danu@gmail.com>
2026-06-15 12:40:50 +0300

    fix: fix params

--- wmsBack/src/main/java/com/isd/wms/repository/OrderRepository.java
+++ wmsBack/src/main/java/com/isd/wms/repository/OrderRepository.java
@@ -1,17 +1,14 @@
 package com.isd.wms.repository;
 
 import com.isd.wms.entity.Order;
-import com.isd.wms.entity.User;
+import com.isd.wms.entity.Task;
 import com.isd.wms.enums.OrderStatus;
-import com.isd.wms.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import org.springframework.data.domain.Pageable;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -20,30 +17,64 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("""
-            SELECT o FROM Order o
-            WHERE (:status IS NULL OR o.logicId = :logicId)
-            AND (:destinationId IS NULL OR o.destinationLocation.id = :destinationId)
-            AND (:status IS NULL OR o.status = :status)
-            AND (:createdAt IS NULL OR o.createdAt = :createdAt)
-            AND (:updatedAt IS NULL OR o.updatedAt = :updatedAt)
-            """)
+        SELECT o FROM Order o
+        WHERE (:status IS NULL OR o.logicId = :logicId)
+        AND (:destinationId IS NULL OR o.destinationLocation.id = :destinationId)
+        AND (:status IS NULL OR o.status = :status)
+        AND (:createdAt IS NULL OR o.createdAt = :createdAt)
+        AND (:updatedAt IS NULL OR o.updatedAt = :updatedAt)
+        """)
     List<Order> filter(
-            @Param("logicId") String logicId,
-            @Param("destinationId") Long destinationId,
-            @Param("status") OrderStatus status,
-            @Param("createdAt") LocalDateTime createdAt,
-            @Param("updatedAt") LocalDateTime updatedAt
+        @Param("logicId") String logicId,
+        @Param("destinationId") Long destinationId,
+        @Param("status") OrderStatus status,
+        @Param("createdAt") LocalDateTime createdAt,
+        @Param("updatedAt") LocalDateTime updatedAt
     );
 
     @Query(value = """
-        SELECT o.* FROM orders o
-        JOIN order_lines ol ON o.id = ol.order_id
-        JOIN tasks t ON ol.task_id = t.id
-        WHERE t.operator_id = :operatorId
-        AND o.status LIKE 'ASSIGNED'
-        LIMIT 1
-    """, nativeQuery = true)
+            SELECT o.* FROM orders o
+            JOIN order_lines ol ON o.id = ol.order_id
+            JOIN tasks t ON ol.task_id = t.id
+            WHERE t.operator_id = :operatorId
+            AND o.status LIKE 'ASSIGNED'
+            LIMIT 1
+        """, nativeQuery = true)
     Optional<Order> findOldestOrderAssignedToOperator(
         @Param("operatorId") Long operatorId
+    );
+
+    @Modifying
+    @Query("""
+            UPDATE Order o
+                SET o.status = :orderStatus
+                WHERE o.id = :orderId
+        """)
+    int updateStatus(
+        @Param("orderId") Long orderId,
+        @Param("orderStatus") OrderStatus orderStatus);
+
+    @Modifying
+    @Query("""
+            UPDATE Order o
+                SET o.status = COMPLETED
+                WHERE o = :order
+                    AND NOT EXISTS (
+                          SELECT 1 FROM OrderLine ol
+                          WHERE ol.order = o
+                            AND ol.status NOT IN (COMPLETED, CANCELED)
+                      )
+        """)
+    int markOrderAsCompleted(
+        @Param("order") Order order
+    );
+
+    @Query("""
+        SELECT o FROM Order o
+        JOIN OrderLine ol ON ol.order = o
+        WHERE ol.task = :task
+    """)
+    Optional<Order> getOrderByTask(
+        @Param("task") Task task
     );
 }
