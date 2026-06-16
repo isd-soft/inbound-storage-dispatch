37d7597a504f72b3e395798da5ff252846991523
Linage <p.anastasia.danu@gmail.com>
2026-06-16 15:51:26 +0300

    chore: exception at validation

--- wmsBack/src/main/java/com/isd/wms/service/InventoryService.java
+++ wmsBack/src/main/java/com/isd/wms/service/InventoryService.java
@@ -26,6 +26,7 @@ import com.isd.wms.repository.ProductRepository;
 import com.isd.wms.repository.StockRepository;
 import com.isd.wms.repository.UserRepository;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -33,6 +34,7 @@ import com.isd.wms.service.imports.ImportService;
 import com.isd.wms.service.imports.xlsx.StockInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -242,7 +244,10 @@ public class InventoryService {
     @Transactional
     public void importStockFromFile(MultipartFile file) {
         List<Stock> stocks = importService.importData(file, StockInfo.class);
-        stocks.stream().forEach((s) -> System.out.println(s.getQuantity()));
-        stockRepository.saveAllAndFlush(stocks);
+        try {
+            stockRepository.saveAllAndFlush(stocks);
+        } catch (DataIntegrityViolationException e) {
+            throw new InvalidRequestException("The imported file contains invalid stock data.");
+        }
     }
 }
