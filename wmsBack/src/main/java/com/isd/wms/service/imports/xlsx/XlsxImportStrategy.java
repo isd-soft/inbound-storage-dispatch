37d23f48e3ddd1433c0a6f6c889a38b8b49d969b
Linage <p.anastasia.danu@gmail.com>
2026-06-24 12:29:30 +0300

    fix: date format

--- wmsBack/src/main/java/com/isd/wms/service/imports/xlsx/XlsxImportStrategy.java
+++ wmsBack/src/main/java/com/isd/wms/service/imports/xlsx/XlsxImportStrategy.java
@@ -46,6 +46,8 @@ public class XlsxImportStrategy implements ImportStrategy {
 
             PoijiOptions options = PoijiOptions.PoijiOptionsBuilder.settings()
                 .preferNullOverDefault(true)
+                .datePattern("dd.MM.yyyy")
+                .dateTimeFormatter(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
                 .build();
 
             PoijiExcelType excelType = PoijiExcelType.XLSX;
